2d7527d2a15ecc8f05c7449c4671af54bb035f5a
Nithika Dias <INT.Nithika@CreativeSoftware.com>
2024-12-20 00:37:10 +0530

    Add video game actions for CRUD operations

---
+++ VideoGameApp/src/actions/videoGameActions.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { VideoGameRequest, VideoGameResponse } from "../types/types";
+import { VideoGameRequestSchema } from "../types/schemas";
+
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+export async function fetchAllVideoGames(): Promise<VideoGameResponse[]> {
+    const response = await axios.get(`${baseURL}/api/VideoGame`);
+    return response.data;
+}
+
+export async function addVideoGame(newGame: VideoGameRequest): Promise<VideoGameResponse> {
+    VideoGameRequestSchema.parse(newGame);
+    return await axios.post(`${baseURL}/api/VideoGame`, newGame);
+}
+
+export async function updateVideoGame(updatedGame: VideoGameRequest, gameId: number): Promise<VideoGameResponse> {
+    VideoGameRequestSchema.parse(updatedGame);
+    console.log(updatedGame);
+    return await axios.put(`${baseURL}/api/VideoGame/${gameId}`, updatedGame);
+}
+
+export async function deleteVideoGame(gameId : number): Promise<void> {
+    return await axios.delete(`${baseURL}/api/VideoGame/${gameId}`);
+}
